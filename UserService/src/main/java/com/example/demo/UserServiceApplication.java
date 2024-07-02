5e957027539b6a863ed25752b8c1ae0b45cce68f
sushma <sushmasarode1998@gmail.com>
2024-07-02 19:38:22 +0530

    Created User Entity

---
+++ UserService/src/main/java/com/example/demo/UserServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cybage.Model.User;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+		
+//		User user = new User();
+//		
+//		user.setId(1);
+//		user.setName("sush");
+//		user.setCity("pune");
+//		
+//		System.out.println(user.toString());
+		
+	}
+
+}
